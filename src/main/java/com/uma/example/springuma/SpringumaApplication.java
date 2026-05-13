3ffdc630e90e0b978298f155e4d8c1fe8a2167bc
unknown <laiadiaz@uma.es>
2026-05-13 04:59:12 +0200

    imports

--- src/main/java/com/uma/example/springuma/SpringumaApplication.java
+++ src/main/java/com/uma/example/springuma/SpringumaApplication.java
@@ -3,6 +3,9 @@ package com.uma.example.springuma;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
